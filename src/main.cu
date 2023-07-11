55db74670467e0fd23f9837b770e27a0dce5c71e
Janusch Patas <janusch.patas@gmx.de>
2023-07-11 10:15:02 +0200

    add tinyply as submodule

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "tinyply.h"
 
 int main(int argc, char* argv[]) {
+    tinyply::PlyFile file;
     std::cout << "Hello gaussian splatting cuda implementation!" << std::endl;
 }
