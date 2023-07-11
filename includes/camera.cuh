15a79d340d6cb2afc3bc34e920ac2ffcca605bea
Janusch Patas <janusch.patas@gmx.de>
2023-07-11 23:37:46 +0200

    point and camera read method implemented

--- includes/camera.cuh
+++ includes/camera.cuh
@@ -1,6 +1,38 @@
-
 #pragma once
 
-class Camera{
+#include <string>
+#include <unordered_map>
+#include <vector>
 
+static const std::unordered_map<int, std::pair<std::string, uint32_t>> camera_model_ids = {
+    {0, {"SIMPLE_PINHOLE", 3}},
+    {1, {"PINHOLE", 4}},
+    {2, {"SIMPLE_RADIAL", 4}},
+    {3, {"RADIAL", 5}},
+    {4, {"OPENCV", 8}},
+    {5, {"OPENCV_FISHEYE", 8}},
+    {6, {"FULL_OPENCV", 12}},
+    {7, {"FOV", 5}},
+    {8, {"SIMPLE_RADIAL_FISHEYE", 4}},
+    {9, {"RADIAL_FISHEYE", 5}},
+    {10, {"THIN_PRISM_FISHEYE", 12}}};
+
+class Camera {
+public:
+    explicit Camera(int model_ID) : _model_ID(model_ID) {
+        _model_name = camera_model_ids.at(_model_ID).first;
+        _params.resize(camera_model_ids.at(_model_ID).second);
+    }
+
+    int GetModelID() const { return _model_ID; }
+
+public:
+    uint32_t _camera_ID;
+    std::string _model_name;
+    uint64_t _width;
+    uint64_t _height;
+    std::vector<double> _params;
+
+private:
+    int _model_ID;
 };
--- src/read_utils.cu
+++ src/read_utils.cu
@@ -1,4 +1,6 @@
+#include "camera.cuh"
 #include "image.cuh"
+#include "point3d.cuh"
 #include "read_utils.cuh"
 #include <fstream>
 #include <iostream>
@@ -60,13 +62,17 @@ T read_binary_value(std::istream& file) {
     return value;
 }
 
-// copied from https://github.com/colmap/colmap/blob/dev/src/colmap/base/reconstruction.cc
+// TODO: Do something with the images vector
+// adapted from https://github.com/colmap/colmap/blob/dev/src/colmap/base/reconstruction.cc
 void read_images_binary(std::filesystem::path file_path) {
-    auto image_stream_buffer = read_binary(file_path / "images.bin");
-    const size_t number_images = read_binary_value<uint64_t>(*image_stream_buffer);
+    auto image_stream_buffer = read_binary(file_path);
+    const size_t image_count = read_binary_value<uint64_t>(*image_stream_buffer);
 
-    for (size_t i = 0; i < number_images; ++i) {
-        Image img;
+    std::vector<Image> images;
+    images.reserve(image_count);
+
+    for (size_t i = 0; i < image_count; ++i) {
+        auto& img = images.emplace_back();
         img._id = read_binary_value<uint32_t>(*image_stream_buffer);
         img._qvec.x() = read_binary_value<double>(*image_stream_buffer);
         img._qvec.y() = read_binary_value<double>(*image_stream_buffer);
@@ -97,6 +103,55 @@ void read_images_binary(std::filesystem::path file_path) {
     }
 }
 
+// TODO: Do something with the cameras vector
+// adapted from https://github.com/colmap/colmap/blob/dev/src/colmap/base/reconstruction.cc
+void read_cameras_binary(std::filesystem::path file_path) {
+    auto camera_stream_buffer = read_binary(file_path);
+    const size_t camera_count = read_binary_value<uint64_t>(*camera_stream_buffer);
+
+    std::vector<Camera> cameras;
+    cameras.reserve(camera_count);
+    for (size_t i = 0; i < camera_count; ++i) {
+        auto camera_ID = read_binary_value<uint32_t>(*camera_stream_buffer);
+        auto model_id = read_binary_value<int>(*camera_stream_buffer);
+        auto& cam = cameras.emplace_back(model_id);
+        cam._width = read_binary_value<uint64_t>(*camera_stream_buffer);
+        cam._height = read_binary_value<uint64_t>(*camera_stream_buffer);
+
+        camera_stream_buffer->read(reinterpret_cast<char*>(cam._params.data()), cam._params.size() * sizeof(double));
+    }
+}
+
+// adapted from https://github.com/colmap/colmap/blob/dev/src/colmap/base/reconstruction.cc
+// TODO: There should be points3D data returned
+void read_point3D_binary(std::filesystem::path file_path) {
+    auto point3D_stream_buffer = read_binary(file_path);
+    const size_t point3D_count = read_binary_value<uint64_t>(*point3D_stream_buffer);
+
+    std::vector<Point3D> points3D;
+    points3D.reserve(point3D_count);
+    for (size_t i = 0; i < point3D_count; ++i) {
+        const auto point3D_ID = read_binary_value<uint64_t>(*point3D_stream_buffer);
+        auto& point = points3D.emplace_back(point3D_ID);
+        point._xyz.x() = read_binary_value<double>(*point3D_stream_buffer);
+        point._xyz.y() = read_binary_value<double>(*point3D_stream_buffer);
+        point._xyz.z() = read_binary_value<double>(*point3D_stream_buffer);
+
+        point._rgb[0] = read_binary_value<uint8_t>(*point3D_stream_buffer);
+        point._rgb[1] = read_binary_value<uint8_t>(*point3D_stream_buffer);
+        point._rgb[2] = read_binary_value<uint8_t>(*point3D_stream_buffer);
+
+        point._error = read_binary_value<double>(*point3D_stream_buffer);
+
+        const size_t track_length = read_binary_value<uint64_t>(*point3D_stream_buffer);
+        point._tracks.resize(track_length);
+        point3D_stream_buffer->read(reinterpret_cast<char*>(point._tracks.data()), track_length * sizeof(Track));
+    }
+}
+
+// TODO: There should be data returned
 void read_colmap_scene_info(std::filesystem::path file_path) {
-    read_images_binary(file_path);
+    read_cameras_binary(file_path / "cameras.bin");
+    read_images_binary(file_path / "images.bin");
+    read_point3D_binary(file_path / "points3D.bin");
 }
