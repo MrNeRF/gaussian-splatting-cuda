87bddfcabc96acab6ca9d34413d5d0f68fd03237
Janusch Patas <janusch.patas@gmx.de>
2023-07-31 20:06:09 +0200

    Introduce torch::indexing::Slice again, create docs

--- includes/gaussian.cuh
+++ includes/gaussian.cuh
@@ -21,11 +21,11 @@ public:
         optimizer = nullptr;
 
         // Scaling activation and its inverse
-        scaling_activation = torch::exp;
-        scaling_inverse_activation = torch::log;
+        _scaling_activation = torch::exp;
+        _scaling_inverse_activation = torch::log;
 
         // Covariance activation function
-        covariance_activation = [](const torch::Tensor& scaling, const torch::Tensor& scaling_modifier, const torch::Tensor& rotation) {
+        _covariance_activation = [](const torch::Tensor& scaling, const torch::Tensor& scaling_modifier, const torch::Tensor& rotation) {
             auto L = build_scaling_rotation(scaling_modifier * scaling, rotation);
             auto actual_covariance = torch::mm(L, L.transpose(1, 2));
             auto symm = strip_symmetric(actual_covariance);
@@ -33,12 +33,67 @@ public:
         };
 
         // Opacity activation and its inverse
-        opacity_activation = torch::sigmoid;
-        inverse_opacity_activation = inverse_sigmoid;
+        _opacity_activation = torch::sigmoid;
+        _inverse_opacity_activation = inverse_sigmoid;
 
         // Rotation activation function
-        rotation_activation = torch::nn::functional::normalize;
+        _rotation_activation = torch::nn::functional::normalize;
     }
+
+public:
+    // Getters
+    torch::Tensor get_xyz() {
+        return _xyz;
+    }
+
+    torch::Tensor get_features() {
+        auto features_dc = _features_dc;
+        auto features_rest = _features_rest;
+        return torch::cat({features_dc, features_rest}, 1);
+    }
+
+    torch::Tensor get_opacity() {
+        return _opacity_activation(_opacity);
+    }
+
+    // torch::Tensor get_covariance(double scaling_modifier = 1.0) {
+    //     return _covariance_activation(get_scaling(), scaling_modifier, _rotation);
+    // }
+
+    // Methods
+    void oneupSHdegree() {
+        if (active_sh_degree < max_sh_degree) {
+            active_sh_degree++;
+        }
+    }
+
+    // void create_from_pcd(BasicPointCloud& pcd, float spatial_lr_scale) {
+    //     this->spatial_lr_scale = spatial_lr_scale;
+
+    //     auto fused_point_cloud = torch::from_blob(pcd.points.data(), {pcd.points.size()}).to(torch::kCUDA);
+    //     auto fused_color = RGB2SH(torch::from_blob(pcd.colors.data(), {pcd.colors.size()}).to(torch::kCUDA));
+
+    //     auto features = torch::zeros({fused_color.size(0), 3, std::pow((max_sh_degree + 1), 2)}).to(torch::kCUDA);
+    //     features.index_put_({torch::indexing::Slice(), torch::indexing::Slice(0,3), 0}, fused_color);
+    //     features.index_put_({torch::indexing::Slice(), torch::indexing::Slice(3), torch::indexing::Slice(1)}, 0.0);
+
+    //     std::cout << "Number of points at initialisation : " << fused_point_cloud.size(0) << std::endl;
+
+    //     auto dist2 = torch::clamp_min(distCUDA2(torch::from_blob(pcd.points.data(), {pcd.points.size()}).to(torch::kCUDA)), 0.0000001);
+    //     auto scales = torch::log(torch::sqrt(dist2)).unsqueeze(-1).repeat({1, 3});
+    //     auto rots = torch::zeros({fused_point_cloud.size(0), 4}).to(torch::kCUDA);
+    //     rots.index_put_({torch::indexing::Slice(), 0}, 1);
+
+    //     auto opacities = inverse_sigmoid(0.5 * torch::ones({fused_point_cloud.size(0), 1}).to(torch::kCUDA));
+
+    //     _xyz = torch::nn::Parameter(fused_point_cloud.requires_grad(true));
+    //     _features_dc = torch::nn::Parameter(features.index({torch::indexing::Slice(), torch::indexing::Slice(), 0}).transpose(1, 2).contiguous().requires_grad(true));
+    //     _features_rest = torch::nn::Parameter(features.index({torch::indexing::Slice(), torch::indexing::Slice(), torch::indexing::Slice(1)}).transpose(1, 2).contiguous().requires_grad(true));
+    //     _scaling = torch::nn::Parameter(scales.requires_grad(true));
+    //     _rotation = torch::nn::Parameter(rots.requires_grad(true));
+    //     _opacity = torch::nn::Parameter(opacities.requires_grad(true));
+    //     _max_radii2D = torch::zeros({_xyz.size(0)}).to(torch::kCUDA);
+    // }
 
 public:
     int active_sh_degree;
@@ -55,14 +110,14 @@ public:
 
     torch::optim::Optimizer* optimizer;
 
-    std::function<torch::Tensor(const torch::Tensor&)> scaling_activation = torch::exp;
-    std::function<torch::Tensor(const torch::Tensor&)> scaling_inverse_activation = torch::log;
+    std::function<torch::Tensor(const torch::Tensor&)> _scaling_activation = torch::exp;
+    std::function<torch::Tensor(const torch::Tensor&)> _scaling_inverse_activation = torch::log;
 
-    std::function<torch::Tensor(const torch::Tensor&, const torch::Tensor&, const torch::Tensor&)> covariance_activation;
+    std::function<torch::Tensor(const torch::Tensor&, const torch::Tensor&, const torch::Tensor&)> _covariance_activation;
 
-    std::function<torch::Tensor(const torch::Tensor&)> opacity_activation = torch::sigmoid;
-    std::function<torch::Tensor(const torch::Tensor&)> inverse_opacity_activation = inverse_sigmoid;
+    std::function<torch::Tensor(const torch::Tensor&)> _opacity_activation = torch::sigmoid;
+    std::function<torch::Tensor(const torch::Tensor&)> _inverse_opacity_activation = inverse_sigmoid;
 
     // Declare function that can hold normalize function
-    std::function<torch::Tensor(const torch::Tensor&, torch::nn::functional::NormalizeFuncOptions)> rotation_activation;
+    std::function<torch::Tensor(const torch::Tensor&, torch::nn::functional::NormalizeFuncOptions)> _rotation_activation;
 };
--- includes/general_utils.cuh
+++ includes/general_utils.cuh
@@ -6,10 +6,23 @@
 #include <random>
 #include <torch/torch.h>
 
+/**
+ * Calculates the inverse sigmoid of a tensor element-wise.
+ *
+ * @param x The input tensor.
+ * @return The tensor with the inverse sigmoid of each element.
+ */
 inline torch::Tensor inverse_sigmoid(torch::Tensor x) {
     return torch::log(x / (1 - x));
 }
 
+/**
+ * Converts an image tensor to a torch tensor with the given resolution.
+ *
+ * @param image The input image tensor.
+ * @param resolution The desired resolution of the output tensor.
+ * @return A torch tensor with the given resolution.
+ */
 inline torch::Tensor ImageToTorch(torch::Tensor image, std::vector<int64_t> resolution) {
     torch::Tensor resized_image = image.resize_(resolution) / 255.0;
     if (resized_image.dim() == 3) {
@@ -19,6 +32,20 @@ inline torch::Tensor ImageToTorch(torch::Tensor image, std::vector<int64_t> reso
     }
 }
 
+/**
+ * @brief A functor that implements an exponential learning rate decay function.
+ *
+ * This functor is used to implement an exponential learning rate decay function. It takes in the initial learning rate,
+ * the final learning rate, the number of steps to delay the decay, the delay multiplier, and the maximum number of steps.
+ *
+ * @param lr_init The initial learning rate.
+ * @param lr_final The final learning rate.
+ * @param lr_delay_steps The number of steps to delay the decay.
+ * @param lr_delay_mult The delay multiplier.
+ * @param max_steps The maximum number of steps.
+ *
+ * @return The learning rate at the given step.
+ */
 struct Expon_lr_func {
     double lr_init;
     double lr_final;
@@ -48,58 +75,82 @@ struct Expon_lr_func {
     }
 };
 
+/**
+ * @brief Strips the lower diagonal elements of a 3x3 matrix and returns them as a 1D tensor of size (N, 6).
+ *
+ * @param L A 3D tensor of size (N, 3, 3) representing a batch of 3x3 matrices.
+ * @return A 2D tensor of size (N, 6) representing the lower diagonal elements of each matrix in L.
+ */
 inline torch::Tensor strip_lowerdiag(torch::Tensor L) {
     torch::Tensor uncertainty = torch::zeros({L.size(0), 6}, torch::dtype(torch::kFloat).device(torch::kCUDA));
+    using Slice = torch::indexing::Slice;
 
-    for (int i = 0; i < L.size(0); ++i) {
-        uncertainty[i][0] = L[i][0][0];
-        uncertainty[i][1] = L[i][0][1];
-        uncertainty[i][2] = L[i][0][2];
-        uncertainty[i][3] = L[i][1][1];
-        uncertainty[i][4] = L[i][1][2];
-        uncertainty[i][5] = L[i][2][2];
-    }
-
+    uncertainty.index_put_({Slice(), 0}, L.index({Slice(), 0, 0}));
+    uncertainty.index_put_({Slice(), 1}, L.index({Slice(), 0, 1}));
+    uncertainty.index_put_({Slice(), 2}, L.index({Slice(), 0, 2}));
+    uncertainty.index_put_({Slice(), 3}, L.index({Slice(), 1, 1}));
+    uncertainty.index_put_({Slice(), 4}, L.index({Slice(), 1, 2}));
+    uncertainty.index_put_({Slice(), 5}, L.index({Slice(), 2, 2}));
     return uncertainty;
 }
 
+/**
+ * @brief Strips the symmetric part of a tensor.
+ *
+ * This function takes an input tensor and returns the tensor with the symmetric part removed.
+ *
+ * @param sym The input tensor.
+ * @return The tensor with the symmetric part removed.
+ */
 inline torch::Tensor strip_symmetric(torch::Tensor sym) {
     return strip_lowerdiag(sym);
 }
 
+/**
+ * @brief Builds a rotation matrix from a tensor of quaternions.
+ *
+ * @param r Tensor of quaternions with shape (N, 4).
+ * @return Tensor of rotation matrices with shape (N, 3, 3).
+ */
 inline torch::Tensor build_rotation(torch::Tensor r) {
     torch::Tensor norm = torch::sqrt(torch::sum(r.pow(2), 1));
     torch::Tensor q = r / norm.unsqueeze(-1);
 
+    using Slice = torch::indexing::Slice;
     torch::Tensor R = torch::zeros({q.size(0), 3, 3}, torch::device(torch::kCUDA));
+    torch::Tensor r0 = q.index({Slice(), 0});
+    torch::Tensor x = q.index({Slice(), 1});
+    torch::Tensor y = q.index({Slice(), 2});
+    torch::Tensor z = q.index({Slice(), 3});
 
-    for (int i = 0; i < q.size(0); ++i) {
-        auto r0 = q[i][0], x = q[i][1], y = q[i][2], z = q[i][3];
-
-        R[i][0][0] = 1 - 2 * (y * y + z * z);
-        R[i][0][1] = 2 * (x * y - r0 * z);
-        R[i][0][2] = 2 * (x * z + r0 * y);
-        R[i][1][0] = 2 * (x * y + r0 * z);
-        R[i][1][1] = 1 - 2 * (x * x + z * z);
-        R[i][1][2] = 2 * (y * z - r0 * x);
-        R[i][2][0] = 2 * (x * z - r0 * y);
-        R[i][2][1] = 2 * (y * z + r0 * x);
-        R[i][2][2] = 1 - 2 * (x * x + y * y);
-    }
-
+    R.index_put_({Slice(), 0, 0}, 1 - 2 * (y * y + z * z));
+    R.index_put_({Slice(), 0, 1}, 2 * (x * y - r0 * z));
+    R.index_put_({Slice(), 0, 2}, 2 * (x * z + r0 * y));
+    R.index_put_({Slice(), 1, 0}, 2 * (x * y + r0 * z));
+    R.index_put_({Slice(), 1, 1}, 1 - 2 * (x * x + z * z));
+    R.index_put_({Slice(), 1, 2}, 2 * (y * z - r0 * x));
+    R.index_put_({Slice(), 2, 0}, 2 * (x * z - r0 * y));
+    R.index_put_({Slice(), 2, 1}, 2 * (y * z + r0 * x));
+    R.index_put_({Slice(), 2, 2}, 1 - 2 * (x * x + y * y));
     return R;
 }
 
+/**
+ * Builds a scaling-rotation matrix from the given scaling and rotation tensors.
+ *
+ * @param s The scaling tensor of shape (N, 3) where N is the number of scaling factors.
+ * @param r The rotation tensor of shape (N, 3) where N is the number of rotation angles.
+ * @return The scaling-rotation matrix of shape (N, 3, 3) where N is the number of scaling-rotation matrices.
+ */
 inline torch::Tensor build_scaling_rotation(torch::Tensor s, torch::Tensor r) {
     torch::Tensor L = torch::zeros({s.size(0), 3, 3}, torch::dtype(torch::kFloat).device(torch::kCUDA));
     torch::Tensor R = build_rotation(r);
 
-    for (int i = 0; i < s.size(0); ++i) {
-        L[i][0][0] = s[i][0];
-        L[i][1][1] = s[i][1];
-        L[i][2][2] = s[i][2];
-    }
+    using Slice = torch::indexing::Slice;
+    L.index_put_({Slice(), 0, 0}, s.index({Slice(), 0}));
+    L.index_put_({Slice(), 1, 1}, s.index({Slice(), 1}));
+    L.index_put_({Slice(), 2, 2}, s.index({Slice(), 2}));
 
-    L = torch::matmul(R, L);
+    L = R.matmul(L);
     return L;
 }
