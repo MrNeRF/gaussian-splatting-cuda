f63ec82b5d2267bf038396ef270f24886c71a36b
Janusch Patas <janusch.patas@gmx.de>
2023-08-23 16:10:32 +0200

    activate loss monitoring

--- src/loss_monitor.cu
+++ src/loss_monitor.cu
@@ -8,11 +8,18 @@ float LossMonitor::Update(float newLoss) {
         _loss_buffer.pop_front();
         _rate_of_change_buffer.pop_front();
     }
-    float rateOfChange = (_loss_buffer.empty()) ? 0.0 : std::abs(newLoss - _loss_buffer.back());
+    const bool buffer_empty = _loss_buffer.empty();
+    const float rateOfChange = buffer_empty ? 0.f : std::abs(newLoss - _loss_buffer.back());
     _rate_of_change_buffer.push_back(rateOfChange);
     _loss_buffer.push_back(newLoss);
-    return std::accumulate(_rate_of_change_buffer.begin(), _rate_of_change_buffer.end(), 0.0) / _rate_of_change_buffer.size();
+
+    // return average rate of change
+    return buffer_empty ? 0.f : std::accumulate(_rate_of_change_buffer.begin(), _rate_of_change_buffer.end(), 0.f) / _rate_of_change_buffer.size();
 }
+
 bool LossMonitor::IsConverging(float threshold) {
-    return Update(_loss_buffer.back()) < threshold;
+    if (_rate_of_change_buffer.size() < _buffer_size) {
+        return false;
+    }
+    return std::accumulate(_rate_of_change_buffer.begin(), _rate_of_change_buffer.end(), 0.f) / _rate_of_change_buffer.size() <= threshold;
 }
