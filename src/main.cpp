b455d6e42389c94a2da57da60d2f48096ea68a53
Janusch Patas <janusch.patas@gmx.de>
2025-06-01 21:04:52 +0200

    simplify argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,34 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    //----------------------------------------------------------------------
-    // 1. Parse command-line arguments
-    //----------------------------------------------------------------------
-    auto args = gs::args::convert_args(argc, argv);
-    gs::param::TrainingParameters params;
-    params.optimization = gs::param::read_optim_params_from_json();
-    if (gs::args::parse_arguments(args, params) < 0)
+    try {
+        // Parse arguments and load parameters in one step
+        auto params = gs::args::parse_args_and_params(argc, argv);
+
+        //----------------------------------------------------------------------
+        // 2. Create dataset
+        //----------------------------------------------------------------------
+        auto dataset = create_camera_dataset(params.dataset);
+        auto scene = dataset->get_scene_info();
+
+        //----------------------------------------------------------------------
+        // 3. Model initialisation
+        //----------------------------------------------------------------------
+        auto splat_data = SplatData::create_from_point_cloud(scene._point_cloud,
+                                                             params.optimization.sh_degree,
+                                                             scene._nerf_norm_radius);
+
+        auto strategy = std::make_unique<InriaADC>(std::move(splat_data));
+
+        //----------------------------------------------------------------------
+        // 4. Create trainer and run training
+        //----------------------------------------------------------------------
+        gs::Trainer trainer(dataset, std::move(strategy), params);
+        trainer.train();
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
         return -1;
-
-    //----------------------------------------------------------------------
-    // 2. Create dataset
-    //----------------------------------------------------------------------
-    auto dataset = create_camera_dataset(params.dataset);
-    auto scene = dataset->get_scene_info();
-
-    //----------------------------------------------------------------------
-    // 3. Model initialisation
-    //----------------------------------------------------------------------
-    auto splat_data = SplatData::create_from_point_cloud(scene._point_cloud,
-                                                         params.optimization.sh_degree,
-                                                         scene._nerf_norm_radius);
-
-    auto strategy = std::make_unique<InriaADC>(std::move(splat_data));
-
-    //----------------------------------------------------------------------
-    // 4. Create trainer and run training
-    //----------------------------------------------------------------------
-    gs::Trainer trainer(dataset, std::move(strategy), params);
-    trainer.train();
-
-    return 0;
+    }
 }
