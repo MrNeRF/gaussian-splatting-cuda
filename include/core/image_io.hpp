bf1a6012eeb7401405da9ea028621257c8f554b0
Janusch Patas <janusch.patas@gmx.de>
2025-06-02 15:19:28 +0200

    add stb_image_write

--- include/core/image_io.hpp
+++ include/core/image_io.hpp
@@ -2,9 +2,11 @@
 
 #include <filesystem>
 #include <future>
+#include <torch/torch.h>
 
 std::tuple<unsigned char*, int, int, int>
 load_image(std::filesystem::path p, int res_div = -1);
+void save_image(const std::filesystem::path& path, torch::Tensor image);
 
 void free_image(unsigned char* image);
 
