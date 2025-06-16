aa3a5d14329961eeee863b4606415a54bf25f8e1
Janusch Patas <janusch.patas@gmx.de>
2025-06-16 23:06:04 +0200

    updates to autograd

--- gsplat/Projection.cpp
+++ gsplat/Projection.cpp
@@ -129,7 +129,7 @@ projection_ewa_3dgs_fused_fwd(
     at::Tensor means2d = at::empty({C, N, 2}, means.options());
     at::Tensor depths = at::empty({C, N}, means.options());
     at::Tensor conics = at::empty({C, N, 3}, means.options());
-    at::Tensor compensations;
+    at::Tensor compensations = {};
     if (calc_compensations) {
         // we dont want NaN to appear in this tensor, so we zero intialize it
         compensations = at::zeros({C, N}, means.options());
@@ -163,29 +163,29 @@ projection_ewa_3dgs_fused_fwd(
 }
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
-projection_ewa_3dgs_fused_bwd(
-    // fwd inputs
-    const at::Tensor means,                // [N, 3]
-    const at::optional<at::Tensor> covars, // [N, 6] optional
-    const at::optional<at::Tensor> quats,  // [N, 4] optional
-    const at::optional<at::Tensor> scales, // [N, 3] optional
-    const at::Tensor viewmats,             // [C, 4, 4]
-    const at::Tensor Ks,                   // [C, 3, 3]
-    const uint32_t image_width,
-    const uint32_t image_height,
-    const float eps2d,
-    const CameraModelType camera_model,
-    // fwd outputs
-    const at::Tensor radii,                       // [C, N, 2]
-    const at::Tensor conics,                      // [C, N, 3]
-    const at::optional<at::Tensor> compensations, // [C, N] optional
-    // grad outputs
-    const at::Tensor v_means2d,                     // [C, N, 2]
-    const at::Tensor v_depths,                      // [C, N]
-    const at::Tensor v_conics,                      // [C, N, 3]
-    const at::optional<at::Tensor> v_compensations, // [C, N] optional
-    const bool viewmats_requires_grad
-) {
+    projection_ewa_3dgs_fused_bwd(
+        // fwd inputs
+        const at::Tensor means,                // [N, 3]
+        const at::optional<at::Tensor> covars, // [N, 6] optional
+        const at::optional<at::Tensor> quats,  // [N, 4] optional
+        const at::optional<at::Tensor> scales, // [N, 3] optional
+        const at::Tensor viewmats,             // [C, 4, 4]
+        const at::Tensor Ks,                   // [C, 3, 3]
+        const uint32_t image_width,
+        const uint32_t image_height,
+        const float eps2d,
+        const CameraModelType camera_model,
+        // fwd outputs
+        const at::Tensor radii,                       // [C, N, 2]
+        const at::Tensor conics,                      // [C, N, 3]
+        const at::optional<at::Tensor> compensations, // [C, N] optional
+        // grad outputs
+        const at::Tensor v_means2d,                     // [C, N, 2]
+        const at::Tensor v_depths,                      // [C, N]
+        const at::Tensor v_conics,                      // [C, N, 3]
+        const at::optional<at::Tensor> v_compensations, // [C, N] optional
+        const bool viewmats_requires_grad
+    ) {
     DEVICE_GUARD(means);
     CHECK_INPUT(means);
     if (covars.has_value()) {
@@ -202,9 +202,11 @@ projection_ewa_3dgs_fused_bwd(
     CHECK_INPUT(v_means2d);
     CHECK_INPUT(v_depths);
     CHECK_INPUT(v_conics);
+
     if (compensations.has_value()) {
         CHECK_INPUT(compensations.value());
     }
+
     if (v_compensations.has_value()) {
         CHECK_INPUT(v_compensations.value());
         assert(compensations.has_value());
