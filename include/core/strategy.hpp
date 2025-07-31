bdbd21866c72152d0f3dd72a4c031201fc1dc64b
MotivaCG <victor@motivacg.com>
2025-07-31 20:31:10 +0200

    Fix MSVS compilation in windows (trick with std::function)

--- include/core/strategy.hpp
+++ include/core/strategy.hpp
@@ -1,11 +1,11 @@
 #pragma once
-
 #include "core/istrategy.hpp"
 #include "core/selective_adam.hpp"
 #include <memory>
 #include <torch/torch.h>
 
 namespace strategy {
+    using OptimizerFnType = std::function<std::unique_ptr<torch::optim::OptimizerParamState>(torch::optim::OptimizerParamState&, const torch::Tensor)>;
     void initialize_gaussians(gs::SplatData& splat_data);
 
     std::unique_ptr<torch::optim::Optimizer> create_optimizer(
@@ -19,7 +19,7 @@ namespace strategy {
 
     void update_param_with_optimizer(
         std::function<torch::Tensor(const int, const torch::Tensor)> param_fn,
-        std::function<std::unique_ptr<torch::optim::OptimizerParamState>((torch::optim::OptimizerParamState&, const torch::Tensor))> optimizer_fn,
+        OptimizerFnType optimizer_fn,
         std::unique_ptr<torch::optim::Optimizer>& optimizer,
         gs::SplatData& splat_data,
         std::vector<size_t> param_idxs = {0, 1, 2, 3, 4, 5});
