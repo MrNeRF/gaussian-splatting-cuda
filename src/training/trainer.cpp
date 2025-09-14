b2938bcf5b10d2c4203dc39fc12d0b7021c05316
kmeirlaen <kristof@kri-soft.be>
2025-09-14 12:40:12 +0200

    expose reality capture and GUT parameters in the GUI

--- src/training/trainer.cpp
+++ src/training/trainer.cpp
@@ -586,11 +586,11 @@ namespace gs::training {
                     return std::unexpected("Training on cameras with ortho model is not supported yet.");
                 }
             } else {
-                // Flag is workaround for non-RC datasets with distortion. By default it is off.
+                // Flag is workaround for non-RC datasets with distortion. By default it is off.                
                 if (!params_.optimization.rc) {
                     if (cam->radial_distortion().numel() != 0 ||
                         cam->tangential_distortion().numel() != 0) {
-                        return std::unexpected("You must use --gut option to train on cameras with distortion.");
+                        return std::unexpected("Distorted images detected.  Enable 'GUT' if true or 'Reality Capture' if applicable.");
                     }
                     if (cam->camera_model_type() != gsplat::CameraModelType::PINHOLE) {
                         return std::unexpected("You must use --gut option to train on cameras with non-pinhole model.");
